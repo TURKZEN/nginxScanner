114ffa9556ea1592cce4281d9bffc80b014c04e4
TURKZEN <batuhanthd@gmail.com>
2023-05-02 22:03:37 +0300

    First commit

---
+++ nginxScanner.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+
+import requests
+from sys import argv,exit
+
+def serverDetection():
+    try:
+        ip = argv[1]
+    except:
+        return "Usage: nginxScanner <IP>"
+        exit()
+    else:
+        try:
+            req = requests.get("http://{}".format(ip))
+            return req.headers.get("server")
+        except:
+            print("Error !")
+def banner():
+    pass
+
+def Main():
+    banner()
+    serverType = serverDetection()
+    print(serverType)
+
+
+
+if __name__ == "__main__":
+    Main()
