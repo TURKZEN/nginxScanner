cd178587df2445a6dd688e25685882985ac6ff71
TURKZEN <batuhanthd@gmail.com>
2023-05-11 11:21:43 +0300

    Request and serverParser added

--- nginxScanner.py
+++ nginxScanner.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+from requests import get
 from ipaddress import ip_address as ipCheck
 from validators import url as urlCheck
 from sys import argv,exit
@@ -16,16 +17,25 @@ def usage():
 
     """)
     exit()
+    
+def serverParser(serverType):
+    print(serverType)
 
-def ip():
+def ip(IP):
     print("IP")
+    req = get("http://{}".format())
+    serverType = req.headers.get("Server")
+    serverParser(serverType)
 
-def url():
+def url(URL):
     print("URL")
-
+    req = get(URL)
+    serverType = req.headers.get("Server")
+    serverParser(serverType)
 
 def verify():
     try:
+        
         IP = argv[1]
     except:
         usage()
@@ -37,10 +47,11 @@ def verify():
         if not urlCheck(IP):
             usage()
         else:
-            url()
+            URL = IP
+            url(URL)
             
     else:
-        ip()
+        ip(IP)
 
 
 def banner():
